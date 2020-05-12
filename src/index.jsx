8ae34fff94a0ee8865655dd99c3b0c5a019b6cd3
GGzor <ggzor@localhost.localdomain>
2020-05-12 00:28:58 -0500

    Adding support for landscape orientation

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,102 @@ import styled from "styled-components"
 
 import { Canvas } from "./canvas/Canvas"
 
+const MainLayout = styled.section`
+  display: grid;
+  grid-template-rows: 1fr auto;
+
+  @media (min-width: 768px) {
+    grid-template-rows: 1fr;
+    grid-template-columns: auto 1fr;
+  }
+
+  @media (max-width: 767px) and (orientation: landscape) {
+    grid-template-rows: 1fr;
+    grid-template-columns: auto 1fr;
+  }
+
+  height: 100%;
+  width: 100%;
+`
+
+const DrawerLayout = styled.section`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr auto;
+
+  grid-column: 1;
+  grid-row: 2;
+
+  @media (max-width: 767px) and (orientation: landscape) {
+    width: 250px;
+    height: 100%;
+    grid-row: 1;
+    grid-column: 1;
+  }
+
+  @media (min-width: 768px) {
+    width: 320px;
+    height: 100%;
+    grid-row: 1;
+    grid-column: 1;
+  }
+
+  border: 1px solid #dbdbdb;
+  padding: 16px;
+`
+
+const DrawerTitle = styled.h2`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 16px;
+  letter-spacing: -0.05em;
+
+  color: rgba(0, 0, 0, 0.7);
+`
+
+const DrawerInfo = styled.p`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: justify;
+
+  color: rgba(0, 0, 0, 0.7);
+`
+
+const DrawerTopItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-row: 1;
+`
+const DrawerBottomItems = styled.div`
+  grid-row: 2;
+`
+
+const CanvasLayout = styled.section`
+  grid-row: 1;
+  grid-column: 1;
+
+  overflow: auto;
+
+  @media (max-width: 767px) and (orientation: landscape) {
+    grid-row: 1;
+    grid-column: 2;
+  }
+
+  @media (min-width: 768px) {
+    grid-row: 1;
+    grid-column: 2;
+  }
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 const App = () => {
   const initialServers = range(5).map((column) => ({
     row: 2,
@@ -63,23 +159,39 @@ const App = () => {
 
   const width = 5
   const height = 4
-  const cellSize = 64
-  const gap = 32
+  const cellSize = 48
+  const gap = 24
 
   return (
-    <Canvas
-      {...{
-        width,
-        height,
-        cellSize,
-        gap,
-        clients,
-        wires,
-        servers,
-        weakWires,
-        regions,
-      }}
-    />
+    <MainLayout>
+      <DrawerLayout>
+        <DrawerTopItems>
+          <DrawerTitle>Read One Write All (ROWA)</DrawerTitle>
+          <DrawerInfo>
+            En esta estrategia de replicación la lectura se puede realizar desde
+            cualquier nodo que se encuentre activo, sin embargo, la escritura
+            sólo se puede llevar a cabo cuando todos los nodos están activos.
+          </DrawerInfo>
+        </DrawerTopItems>
+        <DrawerBottomItems></DrawerBottomItems>
+      </DrawerLayout>
+      <CanvasLayout>
+        {" "}
+        <Canvas
+          {...{
+            width,
+            height,
+            cellSize,
+            gap,
+            clients,
+            wires,
+            servers,
+            weakWires,
+            regions,
+          }}
+        />
+      </CanvasLayout>
+    </MainLayout>
   )
 }
 
